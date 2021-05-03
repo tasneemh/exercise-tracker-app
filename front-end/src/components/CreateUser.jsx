b67bcbfdaece3d6b705bc24113735ab236ea6056
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-05-03 12:55:57 -0400

    add CreateUser.jsx

--- front-end/src/components/CreateUser.jsx
+++ front-end/src/components/CreateUser.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 function CreateUser() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data, e) =>{
+    console.log(data);
+    //history.push("/");
+    e.target.reset();
+  }; 
   return (
-    <div>
-      This is user
+     <div>
+      <h3>Create New User</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input {...register("user")}
+          className="form-control"/>          
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Create New User" className="btn btn-primary"/>
+        </div>
+      </form>
     </div>
   )
 }
