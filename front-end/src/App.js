2931e242f64b4496d6e56a910aa8f8f0bbebe132
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-05-02 20:11:15 -0400

    updated app.js & navbar.jsx

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ExercisesList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route exact path="/" >
+        <ExercisesList />
+      </Route>
+      <Route exact path="/edit/:id" >
+        <EditExercise />
+      </Route>
+      <Route path="/create" >
+        <CreateExercise />
+      </Route>
+      <Route path="/user">
+        <CreateUser />
+      </Route>
+    </Router>
   );
 }
 
